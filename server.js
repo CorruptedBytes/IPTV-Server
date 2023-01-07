ba6446b8177bd22c81f53539c80f33ab07bc6717
Alex <52111292+CorruptedBytes@users.noreply.github.com>
2023-01-08 00:59:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2022 CorruptedBytes
+Copyright (c) 2023 CorruptedBytes
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
